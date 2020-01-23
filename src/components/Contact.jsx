1546b8ced3b61f725d2fbb6ee5636c129e04f685
Your Nameelaine <elaine.mangulabnan@gmail.com>
2020-01-23 12:02:24 -0500

    update content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ function Contact() {
         <ul className= "contact-links">
             <li> <a href="mailto:elaine.mangulabnan@gmail.com">email</a> </li>
             <li> <a href="https://github.com/elainemango">github</a> </li>
-            <li> <a href="https://www.freecodecamp.org/fccbb0b98de-0ff8-462f-a8b7-2abe1612a5bd">freecodecamp</a> </li>
+            <li> <a href="https://www.freecodecamp.org/elainemango">freecodecamp</a> </li>
             <li> <a href="https://www.linkedin.com/in/elaine-mangulabnan/">linkedin</a> </li>
         </ul>
     </div>
