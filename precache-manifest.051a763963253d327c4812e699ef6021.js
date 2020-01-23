b172edd7c60ff260126adf95e72cad74895c92cc
Your Nameelaine <elaine.mangulabnan@gmail.com>
2020-01-23 10:11:55 -0500

    Updates

---
+++ precache-manifest.051a763963253d327c4812e699ef6021.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d3228a2646107ab6a2792e55c1b2147",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "87644d1b8c64145c9952",
+    "url": "/my-portfolio/static/js/2.902196e1.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/my-portfolio/static/js/2.902196e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "084e113e714d26db07c8",
+    "url": "/my-portfolio/static/js/main.450e4d80.chunk.js"
+  },
+  {
+    "revision": "172d0dd7776da14f08f4",
+    "url": "/my-portfolio/static/js/runtime-main.725690a2.js"
+  },
+  {
+    "revision": "5d15bbe50b36c9e62389bce5581e4515",
+    "url": "/my-portfolio/static/media/blog.5d15bbe5.png"
+  },
+  {
+    "revision": "20578d380a0619be13230f05e8b8a1b4",
+    "url": "/my-portfolio/static/media/counter.20578d38.png"
+  },
+  {
+    "revision": "dd295039d42a68f8f53210286357a8b2",
+    "url": "/my-portfolio/static/media/dice.dd295039.png"
+  },
+  {
+    "revision": "bfc2e99aecb6db31f629f50ccf7f0a3a",
+    "url": "/my-portfolio/static/media/emoji.bfc2e99a.png"
+  },
+  {
+    "revision": "29e434ecc6a0531b5b44760a686f30c6",
+    "url": "/my-portfolio/static/media/form.29e434ec.png"
+  },
+  {
+    "revision": "4cc811ccb3f23470b1940f9ca69f6558",
+    "url": "/my-portfolio/static/media/journal.4cc811cc.png"
+  },
+  {
+    "revision": "3192d5efc9afc0e7c4b18fef047e82a7",
+    "url": "/my-portfolio/static/media/landingpage.3192d5ef.png"
+  },
+  {
+    "revision": "216d5610465b51fcfdd195202a23fe79",
+    "url": "/my-portfolio/static/media/newsletter.216d5610.png"
+  },
+  {
+    "revision": "e7c97f5fdf98950584154b4a962cdd01",
+    "url": "/my-portfolio/static/media/notes.e7c97f5f.png"
+  },
+  {
+    "revision": "41230c0ca2b75b97a35e17fef61a202f",
+    "url": "/my-portfolio/static/media/pattern.41230c0c.png"
+  },
+  {
+    "revision": "57fa961499a10eac558ae9d7f432486f",
+    "url": "/my-portfolio/static/media/rgb.57fa9614.png"
+  },
+  {
+    "revision": "635dc1da6ac1b06512d4716fbd5cb0d1",
+    "url": "/my-portfolio/static/media/todolist1.635dc1da.png"
+  },
+  {
+    "revision": "296c4ac9d99ee9c7b505a68487f2d501",
+    "url": "/my-portfolio/static/media/todolist2.296c4ac9.png"
+  },
+  {
+    "revision": "9278a90a931b4c3308cf20d8ff0a5615",
+    "url": "/my-portfolio/static/media/todolist3.9278a90a.png"
+  },
+  {
+    "revision": "d7e33b96558c425235c45e988cb83152",
+    "url": "/my-portfolio/static/media/yelpcamp.d7e33b96.png"
+  }
+]);
